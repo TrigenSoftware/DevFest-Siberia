e23b9b9197de27f2648bc39337daaf73d02c4066
Dan Onoshko <danon0404@gmail.com>
2019-08-14 22:36:09 +0700

    Блок Location  (#14)
    * wip
    
    * location block is done

--- src/App/data/index.ts
+++ src/App/data/index.ts
@@ -3,3 +3,21 @@ export const timezone = '+07:00';
 export const start = `2019-11-29 14:00:00.123${timezone}`;
 
 export const startTime = new Date(start);
+
+export const googleMaps = {
+	keys: {
+		key:      process.env.GOOGLE_MAPS_KEY,
+		language: 'ru'
+	},
+	center: {
+		lat: 54.857755,
+		lng: 83.111595
+	},
+	zoom: 16,
+	options: {
+		draggable:         false,
+		scrollwheel:       false,
+		fullscreenControl: false,
+		mapTypeControl:    true
+	}
+};
