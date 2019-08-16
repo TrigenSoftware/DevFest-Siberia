c531bb3ac0201411087f00c3e0446dbc9cb82281
Rustam <rustamhabibullin1993@mail.ru>
2019-08-16 16:24:27 +0700

    component Team

---
+++ src/App/blocks/Team/Team.tsx
@@ -0,0 +1,121 @@
+import faker from 'faker';
+import React, {
+	Component
+} from 'react';
+import Section, {
+	IProps as ISectionProps
+} from '~/components/Section';
+import ProfileCard from '~/components/ProfileCard';
+import {
+	imageUrl
+} from '@flexis/ui/components/ImageSelect/ImageSelect.stories';
+import stylesheet from './Team.st.css';
+
+export type IProps = ISectionProps;
+
+export default class Team extends Component<ISectionProps> {
+
+	render() {
+
+		const {
+			props
+		} = this;
+
+		return (
+			<Section
+				{...props}
+				{...stylesheet('root', {}, props)}
+			>
+				<h2>Команда</h2>
+				<ul
+					{...stylesheet('list')}
+				>
+					<li
+						{...stylesheet('item')}
+					>
+						<ProfileCard
+							src={imageUrl}
+							firstname='Имя'
+							lastname='Фамилия'
+							description='Роль, занятие, цель смысл'
+							contacts={{
+								twitter: faker.internet.url(),
+								vk:      faker.internet.url()
+							}}
+						/>
+					</li>
+					<li
+						{...stylesheet('item')}
+					>
+						<ProfileCard
+							src={imageUrl}
+							firstname='Имя'
+							lastname='Фамилия'
+							description='Роль, занятие, цель смысл'
+							contacts={{
+								twitter: faker.internet.url(),
+								vk:      faker.internet.url()
+							}}
+						/>
+					</li>
+					<li
+						{...stylesheet('item')}
+					>
+						<ProfileCard
+							src={imageUrl}
+							firstname='Имя'
+							lastname='Фамилия'
+							description='Роль, занятие, цель смысл'
+							contacts={{
+								twitter: faker.internet.url(),
+								vk:      faker.internet.url()
+							}}
+						/>
+					</li>
+					<li
+						{...stylesheet('item')}
+					>
+						<ProfileCard
+							src={imageUrl}
+							firstname='Имя'
+							lastname='Фамилия'
+							description='Роль, занятие, цель смысл'
+							contacts={{
+								twitter: faker.internet.url(),
+								vk:      faker.internet.url()
+							}}
+						/>
+					</li>
+					<li
+						{...stylesheet('item')}
+					>
+						<ProfileCard
+							src={imageUrl}
+							firstname='Имя'
+							lastname='Фамилия'
+							description='Роль, занятие, цель смысл'
+							contacts={{
+								twitter: faker.internet.url(),
+								vk:      faker.internet.url()
+							}}
+						/>
+					</li>
+					<li
+						{...stylesheet('item')}
+					>
+						<ProfileCard
+							src={imageUrl}
+							firstname='Имя'
+							lastname='Фамилия'
+							description='Роль, занятие, цель смысл'
+							contacts={{
+								twitter: faker.internet.url(),
+								vk:      faker.internet.url()
+							}}
+						/>
+					</li>
+				</ul>
+			</Section>
+		);
+	}
+}
