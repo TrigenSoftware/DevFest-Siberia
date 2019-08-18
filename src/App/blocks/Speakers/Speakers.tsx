755c32296ecbf103c462403e969065342b057cd5
Rustam <rustamhabibullin1993@mail.ru>
2019-08-18 15:59:45 +0700

    remove button class

--- src/App/blocks/Speakers/Speakers.tsx
+++ src/App/blocks/Speakers/Speakers.tsx
@@ -33,7 +33,6 @@ export default class Speakers extends Component<ISectionProps> {
 				>
 					<h2>Спикеры</h2>
 					<Button
-						{...stylesheet('button')}
 						variant='secondary'
 					>
 						Стать спикером
