2ac5bd9df7fbea1b1c5660e792077e6d700068a4
Rustam <rustamhabibullin1993@mail.ru>
2019-08-18 15:44:53 +0700

    fix btn

--- src/App/blocks/Subscribe/Subscribe.tsx
+++ src/App/blocks/Subscribe/Subscribe.tsx
@@ -66,6 +66,7 @@ export default class Subscribe extends Component<IProps, IState> {
 						/>
 						<Button
 							{...stylesheet('button')}
+							variant='secondary'
 						>
 							Подписаться
 						</Button>
