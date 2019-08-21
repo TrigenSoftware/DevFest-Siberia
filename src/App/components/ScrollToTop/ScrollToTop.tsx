85ed98b01bbc98b60c41dd40ccf7f3871052dc42
Rustam <rustamhabibullin1993@mail.ru>
2019-08-21 15:58:44 +0700

    add defaultProps

--- src/App/components/ScrollToTop/ScrollToTop.tsx
+++ src/App/components/ScrollToTop/ScrollToTop.tsx
@@ -1,4 +1,5 @@
 import {
+	ReactNode,
 	Component
 } from 'react';
 import {
@@ -6,12 +7,18 @@ import {
 	RouteComponentProps
 } from 'react-router-dom';
 
-export type IProps = RouteComponentProps;
+export interface IProps extends RouteComponentProps {
+	children?: ReactNode;
+}
 
 class ScrollToTop extends Component<IProps> {
 
+	static defaultProps = {
+		children: null
+	};
+
 	render() {
-		return this.props.children || null;
+		return this.props.children;
 	}
 
 	componentDidUpdate({ location: prevLocation }) {
