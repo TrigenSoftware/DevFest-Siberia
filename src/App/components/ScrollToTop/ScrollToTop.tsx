3d1ddc3a40e7c0fff130f3d09a33ed3c3149c651
Rustam <rustamhabibullin1993@mail.ru>
2019-08-21 14:16:46 +0700

    Компонент ScrollToTop (#54)
    * component ScrollToTop
    
    * fix IProps interface
    
    * fix spaces

---
+++ src/App/components/ScrollToTop/ScrollToTop.tsx
@@ -0,0 +1,29 @@
+import {
+	Component
+} from 'react';
+import {
+	withRouter,
+	RouteComponentProps
+} from 'react-router-dom';
+
+export type IProps = RouteComponentProps;
+
+class ScrollToTop extends Component<IProps> {
+
+	render() {
+		return this.props.children;
+	}
+
+	componentDidUpdate({ location: prevLocation }) {
+
+		const {
+			pathname
+		} = this.props.location;
+
+		if (pathname !== prevLocation.pathname) {
+			window.scrollTo(0, 0);
+		}
+	}
+}
+
+export default withRouter(ScrollToTop);
