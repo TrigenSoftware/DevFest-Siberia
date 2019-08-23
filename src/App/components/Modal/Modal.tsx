afc9415d6b70a71f5e8bb1d865d2390d98ae717f
Rustam <rustamhabibullin1993@mail.ru>
2019-08-23 23:17:43 +0700

    fix header click event listener

--- src/App/components/Modal/Modal.tsx
+++ src/App/components/Modal/Modal.tsx
@@ -5,22 +5,12 @@ import {
 	subscribeEvent,
 	Bind
 } from '@flexis/ui/helpers';
-import toggleScrollBlock from '@flexis/ui/components/common/toggleScrollBlock';
-import toggleAriaHide from '@flexis/ui/components/common/toggleAriaHide';
 import FlexisModal, {
 	IProps as IFlexisModalProps
 } from '@flexis/ui/components/Modal';
 import stylesheet from './Modal.st.css';
 
 export type IProps = IFlexisModalProps;
-
-let appElement = null;
-
-export function setAppElement(appElementSource) {
-	appElement = typeof appElementSource === 'string'
-		? document.querySelector(appElementSource)
-		: appElementSource;
-}
 
 export default class Modal extends Component<IProps> {
 
@@ -31,9 +21,7 @@ export default class Modal extends Component<IProps> {
 		transitionDuration: 200
 	};
 
-	private unblockScroll: () => void = null;
-	private unsubscribeKeyDown: () => void = null;
-	private ariaShow: () => void = null;
+	private unsubscribeClick: () => void = null;
 
 	render() {
 
@@ -54,10 +42,6 @@ export default class Modal extends Component<IProps> {
 
 	componentDidMount() {
 		this.toggleEffects();
-	}
-
-	componentWillUnmount() {
-		this.removeEffects();
 	}
 
 	componentDidUpdate({ active: prevActive }: IProps) {
@@ -89,54 +73,23 @@ export default class Modal extends Component<IProps> {
 			active
 		} = this.props;
 
-		this.unblockScroll = toggleScrollBlock(
-			active,
-			this.unblockScroll
-		);
-
-		if (appElement) {
-			this.ariaShow = toggleAriaHide(
-				active,
-				this.ariaShow,
-				appElement
-			);
-		}
-
-		const keyDownSubscribed = typeof this.unsubscribeKeyDown === 'function';
+		const clickSubscribed = typeof this.unsubscribeClick === 'function';
 
 		if (active) {
 
 			const header = document.querySelector('header');
 
-			if (!keyDownSubscribed && header) {
-				this.unsubscribeKeyDown = subscribeEvent(
+			if (!clickSubscribed && header) {
+				this.unsubscribeClick = subscribeEvent(
 					header,
 					'click',
 					this.onHeaderClick
 				);
 			}
 		} else
-		if (keyDownSubscribed) {
-			this.unsubscribeKeyDown();
-			this.unsubscribeKeyDown = null;
-		}
-	}
-
-	private removeEffects() {
-
-		if (typeof this.unblockScroll === 'function') {
-			this.unblockScroll();
-			this.unblockScroll = null;
-		}
-
-		if (typeof this.ariaShow === 'function') {
-			this.ariaShow();
-			this.ariaShow = null;
-		}
-
-		if (typeof this.unsubscribeKeyDown === 'function') {
-			this.unsubscribeKeyDown();
-			this.unsubscribeKeyDown = null;
+		if (clickSubscribed) {
+			this.unsubscribeClick();
+			this.unsubscribeClick = null;
 		}
 	}
 }
