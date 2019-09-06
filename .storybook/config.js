0d4e34d7f32e7927c177024a8378573d6e9e4e9c
dangreen <danon0404@gmail.com>
2019-09-06 13:40:23 +0300

    storyshots

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,18 +1,14 @@
-import React from 'react';
 import './injectTestIcon';
 import {
 	configure
 } from '@trigen/scripts-preset-react-app/storybook/config';
-import {
-	addDecorator
-} from '@storybook/react';
 import '@flexis/ui/reboot.st.css';
 import '../src/App/App.st.css';
 
-addDecorator(story => (
-	<div>
-		{story()}
-	</div>
-));
+const stories = require.context(
+	process.env.PROJECT_SRC,
+	true,
+	/\.stories\.tsx$/
+);
 
-configure(module);
+configure(module, stories);
