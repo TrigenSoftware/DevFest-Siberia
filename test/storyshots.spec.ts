64d851b0e892222382c262055d16b0559130d3e4
dangreen <danon0404@gmail.com>
2019-09-11 01:25:25 +0700

    snapshots

--- test/storyshots.spec.ts
+++ test/storyshots.spec.ts
@@ -1,7 +1,27 @@
 import initStoryshots from '@trigen/scripts-plugin-storybook/jest/storyshots';
+import {
+	toMatchImageSnapshot
+} from 'jest-image-snapshot';
+import {
+	RoutesList
+} from '../src/App/routes';
+
+declare global {
+	namespace jest {
+		// tslint:disable-next-line: interface-name
+		interface Matchers<R> {
+			toMatchImageSnapshot(): R;
+		}
+	}
+	const browser: any;
+}
 
 if (process.platform === 'darwin') {
-	initStoryshots({
+
+	jest.setTimeout(30000);
+	expect.extend({ toMatchImageSnapshot });
+
+	const config = {
 		customizePage(page) {
 			return page.setViewport({
 				width:  1920,
@@ -13,7 +33,51 @@ if (process.platform === 'darwin') {
 				waitUntil: 'networkidle0'
 			};
 		}
+	};
+
+	initStoryshots(config);
+
+	describe.skip('Pages', () => {
+
+		RoutesList.unshift('?noop');
+		RoutesList.push(
+			'/speakers?type=all',
+			'/speakers?id=LeonidKalneus',
+			'/ru/speakers?type=all',
+			'/ru/speakers?id=LeonidKalneus',
+			'/partners?type=all',
+			'/ru/partners?type=all'
+		);
+
+		let page = null;
+
+		beforeAll(async () => {
+			page = await browser.newPage();
+			await config.customizePage(page);
+		});
+
+		afterAll(() => {
+
+			if (page) {
+				return page.close();
+			}
+		});
+
+		RoutesList.forEach((path) => {
+
+			it(path, async () => {
+
+				await page.goto(`http://localhost:3000${path}`, config.getGotoOptions());
+
+				const image = await page.screenshot({
+					fullPage: true
+				});
+
+				expect(image).toMatchImageSnapshot();
+			});
+		});
 	});
+
 } else {
 	it('should skip storyshots', () => {});
 }
