42d1d59cee109fea616a5f84d16919d30d689eaf
Rustam <rustamhabibullin1993@mail.ru>
2019-09-11 15:55:28 +0700

    Исрпвление Stylable стилей (#102)
    * fix/stylable
    
    * fix import
    
    * add st-extends
    
    * snapshots
    
    * fix modal paddings, and team icons
    
    * StylableImportOrderPlugin fullControl
    
    * deps upd
    
    * routeshots

--- test/storyshots.spec.ts
+++ test/storyshots.spec.ts
@@ -1,7 +1,15 @@
 import initStoryshots from '@trigen/scripts-plugin-storybook/jest/storyshots';
+import initRouteshots from '@trigen/scripts-preset-react-app/jest/routeshots';
+import {
+	RoutesList
+} from '../src/App/routes';
 
 if (process.platform === 'darwin') {
-	initStoryshots({
+
+	process.env.SEED = JSON.stringify(584);
+	jest.setTimeout(30000);
+
+	const config = {
 		customizePage(page) {
 			return page.setViewport({
 				width:  1920,
@@ -13,7 +21,20 @@ if (process.platform === 'darwin') {
 				waitUntil: 'networkidle0'
 			};
 		}
-	});
+	};
+
+	initStoryshots(config);
+
+	initRouteshots([
+		...RoutesList,
+		'/speakers?type=all',
+		'/speakers?id=LeonidKalneus',
+		'/ru/speakers?type=all',
+		'/ru/speakers?id=LeonidKalneus',
+		'/partners?type=all',
+		'/ru/partners?type=all'
+	], config);
+
 } else {
 	it('should skip storyshots', () => {});
 }
