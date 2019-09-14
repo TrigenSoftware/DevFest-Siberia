5c7e384d9124ee6e90d81008077c20c2c4c70a05
Rustam <rustamhabibullin1993@mail.ru>
2019-09-14 15:48:32 +0700

    Контейнер Cabinet (#118)
    * component/Cabinet-container
    
    * add locale
    
    * add Route
    
    * add fill translate
    
    * fix ticket size

---
+++ src/App/containers/Cabinet/Cabinet.stories.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import {
+	MemoryRouter
+} from 'react-router';
+import {
+	storiesOf
+} from '@storybook/react';
+import {
+	select
+} from '@storybook/addon-knobs';
+import {
+	I18nProvider
+} from 'i18n-for-react';
+import Header from '~/blocks/Header';
+import Footer from '~/blocks/Footer';
+import ru from '~/locales/ru.json';
+import en from '~/locales/en.json';
+import Cabinet from './';
+
+const stylableApi = `
+Stylable API
+---
+_empty_
+`;
+
+storiesOf('Containers|Cabinet', module)
+	.addParameters({
+		info: stylableApi
+	})
+	.addDecorator(story => (
+		<MemoryRouter initialEntries={['/']}>
+			{story()}
+		</MemoryRouter>
+	))
+	.addDecorator(story => (
+		<div style={{ margin: '-12px' }}>
+			{story()}
+		</div>
+	))
+	.addDecorator(story => (
+		<I18nProvider
+			locale={select('Locale', ['en', 'ru'], 'en')}
+			locales={{
+				ru,
+				en
+			}}
+			objectNotation
+		>
+			{story()}
+		</I18nProvider>
+	))
+	.add(
+		'with default state',
+		() => (
+			<>
+				<Header/>
+				<Cabinet/>
+				<Footer/>
+			</>
+		)
+	);
