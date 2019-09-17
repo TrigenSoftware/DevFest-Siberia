66e9ac2994b7b537bc02c103133715dd91b22343
Rustam <rustamhabibullin1993@mail.ru>
2019-09-17 13:28:05 +0700

    some little changes

--- src/App/blocks/Header/HeaderLoginModal.tsx
+++ src/App/blocks/Header/HeaderLoginModal.tsx
@@ -133,7 +133,13 @@ export class HeaderLoginModal extends Component<IProps, IState> {
 					<FormGroup
 						id='password'
 						label={__`login.password`}
-						notice={<Link>{__x`login.notice`}</Link>}
+						notice={
+							<Link
+								to='/some-link'
+							>
+								{__x`login.notice`}
+							</Link>
+						}
 					>
 						<Input
 							id='password'
@@ -146,7 +152,9 @@ export class HeaderLoginModal extends Component<IProps, IState> {
 						/>
 					</FormGroup>
 					<LoginModalFooter>
-						<Link>
+						<Link
+							to='/'
+						>
 							{__x`login.cancel`}
 						</Link>
 						<Button
