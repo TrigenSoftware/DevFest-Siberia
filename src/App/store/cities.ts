6750574e31a644eb3790e84fef875d01baca14d8
Rustam <rustamhabibullin1993@mail.ru>
2019-09-25 15:27:28 +0700

    Autocomplete для инпутов (#147)
    * enhancement/autocomplete
    
    * remove todo and weather
    
    * pull master fix bug with redirect url
    
    * fix path
    
    * remove last /

--- src/App/store/cities.ts
+++
@@ -1,5 +0,0 @@
-
-export default [
-	'Novosibirsk',
-	'Novokuznetsk'
-];
--- src/App/store/types.ts
+++ src/App/store/types.ts
@@ -2,48 +2,28 @@ import {
 	Record
 } from 'immutable';
 import {
-	WeatherState
-} from './Weather/Weather.types';
-import {
-	WeatherActions
-} from './Weather/Weather.actions';
-import {
-	TodoState
-} from './Todo/Todo.types';
-import {
-	TodoActions
-} from './Todo/Todo.actions';
-import {
 	UserState
 } from './User/User.types';
 import {
 	UserActions
 } from './User/User.actions';
 
-export * from './Weather/Weather.types';
-export * from './Todo/Todo.types';
 export * from './User/User.types';
 
 export * from './User/User.actions';
 
 export interface IStateProps {
-	weather: WeatherState;
-	todo: TodoState;
 	user: UserState;
 }
 
 export interface IActions {
-	weather: WeatherActions;
-	todo: TodoActions;
 	user: UserActions;
 }
 
 type State = ReturnType<Record.Factory<IStateProps>>;
 
 const State = Record<IStateProps>({
-	weather: WeatherState(),
-	todo:    TodoState(),
-	user:    UserState()
+	user: UserState()
 });
 
 export {
