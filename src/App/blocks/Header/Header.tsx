ca61add7f4288511aa20d91375b54c9651d36469
Rustam <rustamhabibullin1993@mail.ru>
2019-09-26 21:20:17 +0700

    remove console.log

--- src/App/blocks/Header/Header.tsx
+++ src/App/blocks/Header/Header.tsx
@@ -191,8 +191,6 @@ export class Header extends Component<IProps> {
 
 			const token = new URLSearchParams(location.search).get('authToken');
 
-			console.log(token);
-
 			await setToken(token);
 
 			history.push({
