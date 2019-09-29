a65d23f1274382a283ca49c1ecf4f99c3e80c035
dangreen <danon0404@gmail.com>
2019-09-29 23:34:29 +0700

    fix timer cat fallback

--- src/App/components/TimerCat/loadable.tsx
+++ src/App/components/TimerCat/loadable.tsx
@@ -1,16 +1,26 @@
 // tslint:disable space-in-parens no-magic-numbers
 import React from 'react';
 import loadable from '@loadable/component';
+import {
+	IProps
+} from './types';
+import fallback from './fallback.jpg';
 
-export default loadable(
+const TimerCat = loadable(
 	() => import(/* webpackChunkName: 'cat' */ './index'),
-	{
-		fallback: (
-			<img
-				width={2900}
-				height={1200}
-				src='data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=='
-			/>
-		)
-	}
+	{ ssr: false }
 );
+
+export default function Loadable(props: IProps) {
+	return (
+		<TimerCat
+			{...props}
+			fallback={
+				<img
+					className={props.className}
+					src={fallback}
+				/>
+			}
+		/>
+	);
+}
