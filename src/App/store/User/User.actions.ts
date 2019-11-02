3bf2ae4d007fe5fbfe2b5d39a311d587cdbb8b64
Rustam <rustamhabibullin1993@mail.ru>
2019-11-02 18:26:26 +0700

    add check for empty redirectURL

--- src/App/store/User/User.actions.ts
+++ src/App/store/User/User.actions.ts
@@ -23,6 +23,13 @@ export abstract class UserActions extends UserReducer.Actions<UserState, State, 
 
 			const redirectUrl = await userService.buy(registrationData);
 
+			if (!redirectUrl) {
+				this.setError({
+					type:  this.buy,
+					error: new Error('Invalid promocode')
+				});
+			}
+
 			if (redirectUrl.startsWith(process.env.API_URL.replace(/\/$/, ''))) {
 				this.setError({
 					type:  this.buy,
